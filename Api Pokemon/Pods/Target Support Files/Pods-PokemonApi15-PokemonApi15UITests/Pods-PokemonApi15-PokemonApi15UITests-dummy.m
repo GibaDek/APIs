7f766b0ad7b3c7b54246092fc37297488e02d295
gilberto Amaro <gibaamaro@gmail.com>
2022-05-02 13:01:50 -0300

    no message

--- Api Pokemon/Pods/Target Support Files/Pods-PokemonApi15-PokemonApi15UITests/Pods-PokemonApi15-PokemonApi15UITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PokemonApi15_PokemonApi15UITests : NSObject
-@end
-@implementation PodsDummy_Pods_PokemonApi15_PokemonApi15UITests
-@end
--- Api Pokemon/PokemonApi15/Model/GenerationV.swift
+++
@@ -1,8 +0,0 @@
-// MARK: - GenerationV
-struct GenerationV: Codable {
-    let blackWhite: Sprites?
-
-    enum CodingKeys: String, CodingKey {
-        case blackWhite = "black-white"
-    }
-}
