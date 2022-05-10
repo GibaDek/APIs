a06ed73fce1fee53e57c3d54ff2c331cfa3a041f
gilberto Amaro <gibaamaro@gmail.com>
2022-05-10 15:02:39 -0300

    no message

--- Pokemon_Api/PokemonApi15/Model/Emerald.swift
+++
@@ -1,9 +0,0 @@
-// MARK: - Emerald
-struct Emerald: Codable {
-    let frontDefault, frontShiny: String?
-
-    enum CodingKeys: String, CodingKey {
-        case frontDefault = "front_default"
-        case frontShiny = "front_shiny"
-    }
-}
--- Pokemon_Api/PokemonApi15/Model/GenerationI.swift
+++
@@ -1,9 +0,0 @@
-// MARK: - GenerationI
-struct GenerationI: Codable {
-    let redBlue, yellow: RedBlue?
-
-    enum CodingKeys: String, CodingKey {
-        case redBlue = "red-blue"
-        case yellow
-    }
-}
